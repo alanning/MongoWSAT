779d58fa6f3336dcf60375284727a9ab9d9ed4fb
Adrian Lanning <alanning@gmail.com>
2011-09-21 15:20:14 -0400

    improve comment

--- MongoProviders/MembershipProvider.cs
+++ MongoProviders/MembershipProvider.cs
@@ -271,10 +271,7 @@ namespace MongoProviders
 		/// <param name="name">The friendly name of the provider.</param>
 		/// <param name="config">A collection of the name/value pairs representing the provider-specific attributes specified in the configuration for this provider.</param>
 		/// <exception cref="T:System.ArgumentNullException">
-		/// The name of the provider is null.
-		/// </exception>
-		/// <exception cref="T:System.ArgumentException">
-		/// The name of the provider has a length of zero.
+		/// The config collection provided is null.
 		/// </exception>
 		/// <exception cref="T:System.InvalidOperationException">
 		/// An attempt is made to call <see cref="M:System.Configuration.Provider.ProviderBase.Initialize(System.String,System.Collections.Specialized.NameValueCollection)"/> on a provider after the provider has already been initialized.
